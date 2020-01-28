a95463dc839d11e8429788ad763070fe95692c16
Renan Basilio <renanbasilio@poli.ufrj.br>
2020-01-28 11:14:36 -0300

    Better loading WIP

--- loader.py
+++ loader.py
@@ -1,6 +1,8 @@
 import cv2 as cv2
 import numpy as np
 import pathlib
+import keras
+import numpy as np
 
 from termcolor import cprint
 
@@ -30,7 +32,6 @@ def load_frame_as_ndarray(cap):
     else:
         return None
 
-
 # Loads the video file in the provided path as an array of frames
 def load_video_as_ndarray(path):
     if path.is_file():
@@ -56,3 +57,45 @@ def load_video_as_ndarray(path):
     else:
         cprint("ERROR: File does not exist '{}'".format(path), 'red')
         return None
+
+
+
+# Classe que carrega dados de maneira assíncrona para o preditor
+# Necessário para reduzir o custo de memória do treinamento do modelo
+# Código adaptado a partir de https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly (https://github.com/afshinea/keras-data-generator)
+class DataGenerator(keras.utils.Sequence):
+    def __init__(self, list_IDs, labels, batch_size=4, dim=(15, 240, 320), n_channels=3, n_classes=len(CLASS_NAMES), shuffle=True):
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def on_epoch_end(self):
+        self.indexes = np.arrange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        Y = np.empty((self.batch_size), dtype=int)
+
+        for i, ID in enumerate(list_IDs_temp):
+            # load data
+            pass
+
+        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+
+    def __len__(self):
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
