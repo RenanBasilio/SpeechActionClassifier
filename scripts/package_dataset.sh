c5085f5d0e547943baa518363e485963d89955b9
Renan Basilio <renanbasilio@poli.ufrj.br>
2020-01-31 16:52:49 -0300

    Change packaging script to output to the directory it's called from

--- scripts/package_dataset.sh
+++ scripts/package_dataset.sh
@@ -2,9 +2,10 @@
 
 if [ -n "$1" ]; then
     echo -e "Packaging dataset..."
+    cwd=$PWD
     cd "$1"
     SIZE=`du -skc unsorted classes.txt splits.txt classes.info | sed '$!d' | cut -f 1`
-    tar cf - unsorted classes.txt splits.txt classes.info -P | pv -s ${SIZE}k | gzip > D3PERJ-COPPETEC-$(date +'%Y-%m-%d').tar.gz
+    tar cf - unsorted classes.txt splits.txt classes.info -P | pv -s ${SIZE}k | gzip > $cwd/D3PERJ-COPPETEC-$(date +'%Y-%m-%d').tar.gz
     echo -e "Done."
 else
     echo -e "Usage: package_dataset.sh <dir>"
