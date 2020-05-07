9a2063237ff916fddab971b9816ba419b0b09399
Renan <renanbasilio@poli.ufrj.br>
2020-05-07 00:57:55 -0300

    Add dropout

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import InputLayer, Dense, Flatten, Conv3D, Dropout, MaxPooling3D, AveragePooling3D
+from tensorflow.keras.layers import InputLayer, Dense, Flatten, Conv3D, Dropout, SpatialDropout3D, MaxPooling3D, AveragePooling3D
 
 classes = ['Idle', 'Speak']
 colormode = 'landmarks'
@@ -13,9 +13,11 @@ def get_model(input_shape):
         Conv3D(32, (3, 3, 3), activation='relu'),
         MaxPooling3D(),
         Conv3D(64, (1, 3, 3), activation='relu'),
+        SpatialDropout3D(0.4),
         MaxPooling3D(),
         Flatten(),
-        Dense(512, activation='relu'),
         Dense(256, activation='relu'),
+        Dropout(0.5),
+        Dense(128, activation='relu'),
         Dense(2, activation='softmax')
     ])
