4c8d51888d181533e050e4204e13a7c0cf84fd78
Renan Basilio <renanbasilio@poli.ufrj.br>
2020-05-08 16:31:57 -0300

    Add bias regularizer to layer 2

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import InputLayer, Dense, Flatten, Conv3D, Dropout, SpatialDropout3D, MaxPooling3D, AveragePooling3D
+from tensorflow.keras.regularizers import l1, l2, l1_l2
 
 classes = ['Idle', 'Speak']
 colormode = 'landmarks'
@@ -8,16 +9,16 @@ optical_flow = False
 def get_model(input_shape):
     return Sequential([
         InputLayer(input_shape=input_shape),
-        Conv3D(16, (4, 3, 3), activation='relu'),
+        Conv3D(16, (4, 3, 3), activation='relu', name="conv3d_0"),
         MaxPooling3D(),
-        Conv3D(32, (3, 3, 3), activation='relu'),
+        Conv3D(32, (3, 3, 3), activation='relu', bias_regularizer=l1(0.01), name="conv3d_1"),
         MaxPooling3D(),
-        Conv3D(64, (1, 3, 3), activation='relu'),
-        SpatialDropout3D(0.5),
+        Conv3D(64, (1, 3, 3), activation='relu', name="conv3d_2"),
+        SpatialDropout3D(0.4),
         MaxPooling3D(),
         Flatten(),
-        Dense(256, activation='relu'),
-        Dropout(0.7),
-        Dense(128, activation='relu'),
-        Dense(2, activation='softmax')
+        Dense(256, activation='relu', name="dense_0"),
+        Dropout(0.5),
+        Dense(128, activation='relu', name="dense_1"),
+        Dense(2, activation='softmax', name="softmax")
     ])
