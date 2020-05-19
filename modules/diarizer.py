2e55dcc7a64f02639789da03e7dd2fa9f889ac81
Renan <renanbasilio@poli.ufrj.br>
2020-05-18 21:14:58 -0300

    Implement gaussian weighted commit

--- modules/diarizer.py
+++ modules/diarizer.py
@@ -1,15 +1,27 @@
-import tensorflow as tf
 import cv2 as cv2
 import numpy as np
 import pathlib
+import math
+import functools
 from modules.loader import compute_facial_landmarks
-
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus:
-    tf.config.experimental.set_memory_growth(gpu, True)
+from tensorflow.keras.models import load_model
 
 gauss_weights = np.array([
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    0.000133830225764885,
+    0.00111769186648184,
+    0.00673409947523024,
+    0.0292702560291608, 
+    0.0917831739641082, 
+    0.207629558115201, 
+    0.338847935756788,
+    0.398942280401433, 
+    0.338847935756788, 
+    0.207629558115201, 
+    0.0917831739641082, 
+    0.0292702560291608, 
+    0.00673409947523025, 
+    0.00111769186648185, 
+    0.000133830225764885
 ])
 
 # This commit strategy takes the sum of the elements for each column in the input array
@@ -34,15 +46,43 @@ def strat_freq(array):
         else:
             return np.argmax(argc)
 
-# This commit strategy takes the frequency with which each class in the input array would
-# be selected, weighted using a gaussian distribution, and chooses the most frequent class, 
-# or -1 if both are equal.
-def strat_gaussf(array):
-    pass
+# This commit strategy assigns a weight to the probability of each class in the input array,
+# and chooses the class with the highest weighted probability
+def strat_weighted(array, weights):
+    if len(array) > 0:
+        weighted_sums = gauss_weights[:len(array)].dot(array)
+        if weighted_sums[0] == weighted_sums[1]:
+            return -1
+        else:
+            return np.argmax(weighted_sums)
+
+# This commit strategy assigns a weight to each predicted class in the input array, and
+# chooses the most frequent class by the weighted sum of the predictions.
+def strat_weightedfreq(array, weights):
+    if len(array) > 0:
+        argc = np.zeros(len(array[0][0]))
+        for index, e in enumerate(array):
+            argc[np.argmax(e)] += weights[index]
+        if argc[0] == argc[1]:
+            return -1
+        else:
+            return np.argmax(argc)
+
+# This function computes a set of count gaussian weights centered on x=0 and with standard 
+# deviation equal to sigma (default 2). The returned weights are distributed linearly within 
+# 2 standard deviations.
+def init_gauss_weights(count, sigma=2):
+    gauss_weights = []
+    for i in np.linspace(-(2*sigma), (2*sigma), count):
+        weight = math.pow(math.e, -(math.pow(i, 2)/2(math.pow(sigma), 2))) / (sigma*math.sqrt(2 * math.pi))
+        gauss_weights.append(weight)
+    return np.asarray(gauss_weights)
 
 class Diarizer():
     def __init__(self, model, commit_strategy="sum", shift=1):
-        self.__predictor = tf.keras.models.load_model(model)
+        assert isinstance(shift, int) and shift > 0
+
+        self.__predictor = load_model(model)
         self.__shift = shift
 
         if commit_strategy is "sum":
@@ -50,7 +90,7 @@ class Diarizer():
         elif commit_strategy is "freq":
             self.__commit_strategy = strat_freq
         elif commit_strategy is "gaussf":
-            self.__commit_strategy = strat_gaussf
+            self.__commit_strategy = functools.partial(strat_weightedfreq, weights=init_gauss_weights(15 / shift))
         elif callable(commit_strategy):
             self.__commit_strategy = commit_strategy
         else:
