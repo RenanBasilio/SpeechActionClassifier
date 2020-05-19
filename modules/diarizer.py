bf830107a0cf66222cac5021ded277cba0fe7915
Renan <renanbasilio@poli.ufrj.br>
2020-05-19 03:10:20 -0300

    Output transcription to rttm and diar formats

--- modules/diarizer.py
+++ modules/diarizer.py
@@ -6,95 +6,34 @@ import functools
 from modules.loader import compute_facial_landmarks
 from tensorflow.keras.models import load_model
 
-gauss_weights = np.array([
-    0.000133830225764885,
-    0.00111769186648184,
-    0.00673409947523024,
-    0.0292702560291608, 
-    0.0917831739641082, 
-    0.207629558115201, 
-    0.338847935756788,
-    0.398942280401433, 
-    0.338847935756788, 
-    0.207629558115201, 
-    0.0917831739641082, 
-    0.0292702560291608, 
-    0.00673409947523025, 
-    0.00111769186648185, 
-    0.000133830225764885
-])
-
-# This commit strategy takes the sum of the elements for each column in the input array
-# and chooses the column with the highest total, or -1 if both are equal.
-def strat_sum(array):
-    if len(array) > 0:
-        sum_v = np.sum(array, axis=0)
-        if sum_v[0][0] == sum_v[0][1]:
-            return -1
-        else:
-            return np.argmax(sum_v[0])
-
-# This commit strategy takes the frequency with which each class in the input array would
-# be selected and chooses the most frequent class, or -1 if both are equal.
-def strat_freq(array):
-    if len(array) > 0:
-        argc = np.zeros(len(array[0][0]))
-        for e in array:
-            argc[np.argmax(e)] += 1
-        if argc[0] == argc[1]:
-            return -1
-        else:
-            return np.argmax(argc)
-
-# This commit strategy assigns a weight to the probability of each class in the input array,
-# and chooses the class with the highest weighted probability
-def strat_weighted(array, weights):
-    if len(array) > 0:
-        weighted_sums = gauss_weights[:len(array)].dot(array)
-        if weighted_sums[0] == weighted_sums[1]:
-            return -1
-        else:
-            return np.argmax(weighted_sums)
-
-# This commit strategy assigns a weight to each predicted class in the input array, and
-# chooses the most frequent class by the weighted sum of the predictions.
-def strat_weightedfreq(array, weights):
-    if len(array) > 0:
-        argc = np.zeros(len(array[0][0]))
-        for index, e in enumerate(array):
-            argc[np.argmax(e)] += weights[index]
-        if argc[0] == argc[1]:
-            return -1
-        else:
-            return np.argmax(argc)
-
-# This function computes a set of count gaussian weights centered on x=0 and with standard 
-# deviation equal to sigma (default 2). The returned weights are distributed linearly within 
-# 2 standard deviations.
-def init_gauss_weights(count, sigma=2):
-    gauss_weights = []
-    for i in np.linspace(-(2*sigma), (2*sigma), count):
-        weight = math.pow(math.e, -(math.pow(i, 2)/2(math.pow(sigma), 2))) / (sigma*math.sqrt(2 * math.pi))
-        gauss_weights.append(weight)
-    return np.asarray(gauss_weights)
+class Segment():
+    def __init__(self, value=None, onset=None, duration=None, confidence=None):
+        self.value = value
+        self.onset = onset
+        self.duration = duration
+        self.confidence = confidence
+    
+    def __str__(self):
+        return "CLASS: {} | ONSET: {:.3f} s | DURATION: {:.3f} s | CONFIDENCE: {:.3f}".format(self.value, self.onset, self.duration, self.confidence)
 
 class Diarizer():
-    def __init__(self, model, commit_strategy="sum", shift=1):
+    def __init__(self, model, commit_strategy="mean", shift=1):
         assert isinstance(shift, int) and shift > 0
 
         self.__predictor = load_model(model)
         self.__shift = shift
 
-        if commit_strategy is "sum":
-            self.__commit_strategy = strat_sum
+        if commit_strategy is "mean":
+            self.__commit_strategy = strat_mean
         elif commit_strategy is "freq":
             self.__commit_strategy = strat_freq
         elif commit_strategy is "gaussf":
-            self.__commit_strategy = functools.partial(strat_weightedfreq, weights=init_gauss_weights(15 / shift))
+            gauss_weights = init_gauss_weights(math.floor(15 / shift)) 
+            self.__commit_strategy = functools.partial(strat_weightedfreq, weights=gauss_weights)
         elif callable(commit_strategy):
             self.__commit_strategy = commit_strategy
         else:
-            raise TypeError("commit_strategy can only be either a string in ['sum', 'freq', 'gaussf'] or a callable method taking a numpy 2d array and returning the integer index of the class.")
+            raise TypeError("commit_strategy can only be either a string in ['mean', 'freq', 'gaussf'] or a callable method taking a numpy 2d array and returning the integer index of the class.")
 
     def diarize(self, video, progress_callback=None):
         capture = cv2.VideoCapture(video)
@@ -125,8 +64,8 @@ class Diarizer():
                         predictions.append([])
                     else:
                         # If a frame failed to load or preprocess, commit the ones before it if possible
+                        predictions.append([np.zeros((1, 2))])
                         segments = self.__commit_results(segments, predictions)
-                        segments.append(-1)
                         predictions.clear()
                         window.clear()
                 else:
@@ -148,13 +87,37 @@ class Diarizer():
             if done:
                 segments = self.__commit_results(segments, predictions)
 
+        segments = self.__finalize(segments, {'FPS':capture.get(cv2.CAP_PROP_FPS)})
         return segments
 
     def __commit_results(self, transcription, prediction_window):
         # For each frame in the prediction window
         for result_array in prediction_window:
-            transcription.append(self.__commit_strategy(result_array))
+            if len(result_array) > 0:
+                # Compute predictions for each class based on the commit strategy
+                confidences = self.__commit_strategy(result_array)
+                if confidences[0] == confidences[1]:
+                    result = -1
+                    confidence = 0
+                else:
+                    result = np.argmax(confidences)
+                    confidence = confidences[result]
+            else:
+                result = -1
+                confidence = 0
 
+            if len(transcription) > 0 and transcription[-1].value == result:
+                # If result same as last segment type, extend it by one frame
+                transcription[-1].duration += 1
+                transcription[-1].confidence.append(confidence)
+            else:
+                # Otherwise start a new segment
+                transcription.append(Segment(
+                    result, 
+                    onset=(0 if len(transcription) == 0 else (transcription[-1].onset + transcription[-1].duration)),
+                    duration=1,
+                    confidence=[confidence]
+                ))
         return transcription
 
     def __preprocess_frame(self, frame):
@@ -165,4 +128,58 @@ class Diarizer():
         if frame is not None:
             frame = np.expand_dims(frame, axis=2)
         return frame
-            
+
+    def __finalize(self, transcription, props):
+        for segment in transcription:
+            segment.onset = segment.onset / props['FPS']
+            segment.duration = segment.duration / props['FPS']
+            segment.confidence = np.mean(segment.confidence)
+        return transcription
+
+# This commit strategy takes the sum of the elements for each column in the input array
+# and chooses the column with the highest total, or -1 if both are equal.
+def strat_mean(array):
+    if len(array) > 0:
+        mean_v = np.mean(array, axis=0)
+        return mean_v[0]
+
+# This commit strategy takes the frequency with which each class in the input array would
+# be selected and chooses the most frequent class, or -1 if both are equal.
+def strat_freq(array):
+    if len(array) > 0:
+        argc = np.zeros(len(array[0][0]))
+        for e in array:
+            argc[np.argmax(e)] += 1
+        return argc
+
+# This commit strategy assigns a weight to the probability of each class in the input array,
+# and chooses the class with the highest weighted probability
+def strat_weighted(array, weights):
+    if len(array) > 0:
+        weighted_sums = weights[:len(array)].dot(array)
+        return weighted_sums
+
+# This commit strategy assigns a weight to each predicted class in the input array, and
+# chooses the most frequent class by the weighted sum of the predictions.
+def strat_weightedfreq(array, weights):
+    if len(array) > 0:
+        argc = np.zeros(len(array[0][0]))
+        for index, e in enumerate(array):
+            if e[0][0] == e[0][1]:
+                argc[0] = 0
+                argc[1] = 0
+            else:
+                argc[np.argmax(e)] += weights[index]
+        return argc
+
+# This function computes a set of gaussian weights centered on x=0 and with standard deviation sigma=1. 
+# The returned weights are distributed linearly within the given span and scaled to add up to 1.
+def init_gauss_weights(count, span=(-2, 2)):
+    gauss_weights = []
+    X, step = np.linspace(span[0], span[1], count, retstep=True)
+    for x in X:
+        weight = math.pow(math.e, -(math.pow(x, 2)/2)) * step / math.sqrt(2 * math.pi)
+        gauss_weights.append(weight)
+    gauss_weights = np.asarray(gauss_weights)
+    gauss_weights *= 1 / np.sum(gauss_weights)
+    return gauss_weights
--- test.py
+++ test.py
@@ -1,21 +1,35 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
-
-from modules.diarizer import Diarizer
-from modules.utils import print_progress
 import pathlib
 import sys
 import time
 import datetime
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
 
-diarizer = Diarizer(pathlib.Path("models/2020-04-05-1754_model.h5").absolute(), commit_strategy="sum", shift=1)
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+from modules.diarizer import Diarizer
+from modules.utils import print_progress
+
+diarizer = Diarizer(pathlib.Path("models/2020-04-05-1754_model.h5").absolute(), commit_strategy="gaussf", shift=1)
 
 if __name__ == "__main__":
     start_time = time.time()
 
     segments = diarizer.diarize(sys.argv[1], progress_callback=print_progress)
-    with open(sys.argv[1] + '.diar', 'w') as f:
-        for item in segments:
-            f.write("%s\n" % item)
+    diar = open(sys.argv[1] + '.diar', 'w')
+    rttm = open(sys.argv[1] + '.rttm', 'w')
+    for item in segments:
+        diar.write("{}\n".format(item))
+        if item.value == 1:
+            rttm.write("SPEAKER {file} 1 {turn_onset:.3f} {turn_dur:.3f} <NA> <NA> speaker <NA> <NA>\n".format(
+                file=os.path.basename(sys.argv[1]).split('.')[0],
+                turn_onset=item.onset,
+                turn_dur=item.duration
+            ))
+    diar.close()
+    rttm.close()
 
     print("--- Took {} s ---            ".format(time.time() - start_time))
