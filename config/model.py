13f77a36ce5a6ae3ecca4e8335e781dc38759965
Renan Basilio <renanbasilio@poli.ufrj.br>
2020-05-21 01:14:59 -0300

    Increase dropout
    Best val_loss:   Epoch 13 ( val_loss: 0.3844 / val_acc: 0.8687 / f1_score: 0.8636 / auc: 0.9400 )
    Best val_acc:    Epoch  8 ( val_loss: 0.3929 / val_acc: 0.8732 / f1_score: 0.9329 / auc: 0.9329 )
    End of Training: Epoch 63 ( val_loss: 0.4865 / val_acc: 0.8535 / f1_score: 0.8566 / auc: 0.9320 )

--- config/model.py
+++ config/model.py
@@ -9,16 +9,19 @@ optical_flow = False
 def get_model(input_shape):
     return Sequential([
         InputLayer(input_shape=input_shape),
-        Conv3D(16, (4, 3, 3), activation='relu', name="conv3d_0"),
+        Conv3D(16, (4, 3, 3), activation='relu', bias_regularizer=l2(0.0001), kernel_regularizer=l2(0.0001), name="conv3d_0"),
+	SpatialDropout3D(0.4),
         MaxPooling3D(),
-        Conv3D(32, (3, 3, 3), activation='relu', bias_regularizer=l1(0.01), name="conv3d_1"),
-        MaxPooling3D(),
-        Conv3D(64, (1, 3, 3), activation='relu', name="conv3d_2"),
+        Conv3D(32, (3, 3, 3), activation='relu', bias_regularizer=l2(0.0001), kernel_regularizer=l2(0.0001), name="conv3d_1"),
         SpatialDropout3D(0.4),
         MaxPooling3D(),
+        Conv3D(64, (1, 3, 3), activation='relu', bias_regularizer=l2(0.0001), kernel_regularizer=l2(0.0001), name="conv3d_2"),
+        SpatialDropout3D(0.35),
+        MaxPooling3D(),
         Flatten(),
-        Dense(256, activation='relu', name="dense_0"),
+        Dense(256, activation='relu', bias_regularizer=l2(0.0001), kernel_regularizer=l2(0.0001), name="dense_0"),
         Dropout(0.5),
-        Dense(128, activation='relu', name="dense_1"),
-        Dense(2, activation='softmax', name="softmax")
+        Dense(128, activation='relu', bias_regularizer=l2(0.0001), kernel_regularizer=l2(0.0001), name="dense_1"),
+        Dropout(0.25),
+        Dense(2, activation='softmax', bias_regularizer=l2(0.0001), kernel_regularizer=l2(0.0001), name="softmax")
     ])
