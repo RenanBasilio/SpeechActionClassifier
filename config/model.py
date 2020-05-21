b2dca44d042ff48bdb485794423fe9981f09dcbd
Renan <renanbasilio@poli.ufrj.br>
2020-05-21 17:10:31 -0300

    Revert "Merge remote-tracking branch 'origin/master' into Model_TypeA"
    This reverts commit 4bb6baa6ed3c9d6038f422bcd8f979c6ea3f6bc7, reversing
    changes made to 7c6f4faebd36efdb9161856bcb48ee028317470b.

--- config/model.py
+++ config/model.py
@@ -1,6 +1,5 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import InputLayer, Dense, Flatten, Conv3D, Dropout, SpatialDropout3D, MaxPooling3D, AveragePooling3D
-from tensorflow.keras.regularizers import l1, l2, l1_l2
+from tensorflow.keras.layers import InputLayer, Dense, Flatten, Conv3D, Dropout, MaxPooling3D, AveragePooling3D
 
 classes = ['Idle', 'Speak']
 colormode = 'landmarks'
@@ -9,19 +8,14 @@ optical_flow = False
 def get_model(input_shape):
     return Sequential([
         InputLayer(input_shape=input_shape),
-        Conv3D(16, (4, 3, 3), activation='relu', bias_regularizer=l2(0.0001), kernel_regularizer=l2(0.0001), name="conv3d_0"),
-	SpatialDropout3D(0.4),
+        Conv3D(16, (4, 3, 3), activation='relu'),
         MaxPooling3D(),
-        Conv3D(32, (3, 3, 3), activation='relu', bias_regularizer=l2(0.0001), kernel_regularizer=l2(0.0001), name="conv3d_1"),
-        SpatialDropout3D(0.4),
+        Conv3D(32, (3, 3, 3), activation='relu'),
         MaxPooling3D(),
-        Conv3D(64, (1, 3, 3), activation='relu', bias_regularizer=l2(0.0001), kernel_regularizer=l2(0.0001), name="conv3d_2"),
-        SpatialDropout3D(0.35),
+        Conv3D(64, (1, 3, 3), activation='relu'),
         MaxPooling3D(),
         Flatten(),
-        Dense(256, activation='relu', bias_regularizer=l2(0.0001), kernel_regularizer=l2(0.0001), name="dense_0"),
-        Dropout(0.5),
-        Dense(128, activation='relu', bias_regularizer=l2(0.0001), kernel_regularizer=l2(0.0001), name="dense_1"),
-        Dropout(0.25),
-        Dense(2, activation='softmax', bias_regularizer=l2(0.0001), kernel_regularizer=l2(0.0001), name="softmax")
+        Dense(512, activation='relu'),
+        Dense(256, activation='relu'),
+        Dense(2, activation='softmax')
     ])
