bbac2fe1944522c2fdce7735c6c34e97ef34f674
Renan <renanbasilio@poli.ufrj.br>
2020-05-21 17:17:11 -0300

    Stop tensorflow from using all of the GPU memory

--- diarize.py
+++ diarize.py
@@ -57,6 +57,10 @@ def main(args):
         print("Diarizing {}...".format(vidpath))
 
     import tensorflow as tf
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+        
     from modules.diarizer import Diarizer
 
     diarizer = Diarizer(pathlib.Path(args.model.name).absolute(), commit_strategy=args.strat, shift=args.step, classes=classes)
